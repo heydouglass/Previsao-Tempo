b6351f20c588beac065b7687bb48ff901779ca75
heyDouglas <doguiyt@gmail.com>
2023-10-06 16:44:58 -0300

    nha

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Previsao-Tempo
- Uma pequena aplicação de Previsão do Tempo 
+# Previsão do Tempo
+
+Uma aplicação de Previsão do Tempo.
+
+## Índice
+
+- [Visão Geral](#visão-geral)
+- [Funcionalidades](#funcionalidades)
+- [Requisitos do Sistema](#requisitos-do-sistema)
+- [Instalação](#instalação)
+- [Uso](#uso)
+- [Contribuição](#contribuição)
+- [Licença](#licença)
+
+## Visão Geral
+
+Uma aplicação de Previsão do Tempo. Feita em Python.
+
+## Funcionalidades
+
+- Verifica a temperatura atual da Cidade selecionada.
+- Verifica a umidade atual da Cidade selecionada.
+- Da uma pequena descrição sobre o clima da Cidade selecionada.
+
+## Requisitos do Sistema
+
+- Navegador da web moderno
+
+## Uso
+
+Apenas digite o nome da Cidade que deseja verificar o Clima e clique em buscar.
---
+++ app.py
@@ -0,0 +1,46 @@
+import requests
+import json
+from decouple import config
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+API_KEY = config("API_KEY")
+
+# Carrega o arquivo JSON com os dados das cidades
+with open('static/cities.json', 'r', encoding='utf-8') as json_file:
+    city_data = json.load(json_file)
+
+cities = [city["name"] for city in city_data]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/autocomplete', methods=['GET', 'POST'])
+def autocomplete():
+    search_term = request.args.get('term')
+    matched_cities = [
+        city for city in cities if search_term.lower() in city.lower()]
+    return jsonify(matched_cities)
+
+
+@app.route('/processar_dados', methods=['POST'])
+def processar_dados():
+    cidade = request.form.get('cidade')
+    link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={API_KEY}&lang=pt_br"
+
+    requisicao = requests.get(link)
+    requisicao_dic = requisicao.json()
+    descricao = requisicao_dic['weather'][0]['description']
+    temperatura = requisicao_dic['main']['temp'] - 273.15
+    umidade = requisicao_dic['main']['humidity']
+
+    temperatura_int = int(temperatura)
+    return render_template('index.html', descricao=descricao, temperatura_int=temperatura_int, umidade=umidade, cidade=cidade)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
